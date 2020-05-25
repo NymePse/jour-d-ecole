c1ccc049f498dd1a8025cb876f56d74a9a18f0a4
Pseudo_Nyme <arnaud.bijou@gmail.com>
2020-05-25 14:50:31 +0200

    Constantes de requêtes JQuery

--- js/game.js
+++ js/game.js
@@ -1,5 +1,18 @@
+//Sélecteurs élements Jquery
+const choixMode = "#mode";
+const choixExo = "#exercice";
+const choixDiff = "#difficulte";
+const choixChrono = "#chrono";
+const bouton = "#jouer";
+const jeu =  "#jeu";
+const haut = "#haut";
+const gauche = "#gauche";
+const bas = "#bas";
+const droite = "#droite";
+const espace = "#espace";
+
 var gameArea = {
-    canvas : document.getElementsByTagName("canvas");
+    canvas : $(jeu),
     start : function() {
         this.context = this.canvas.getContext("2d");
         this.interval = setInterval()
@@ -11,5 +24,4 @@ function setGame() {
      * Récupère les options
      * et lance une partie en fonction
      */
-    console.log(choixMode.options[choixMode.selected])
 }
