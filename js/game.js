df8e1c9d9eb47671dffbe8331bdef42fc377ba0a
Pseudo_Nyme <arnaud.bijou@gmail.com>
2020-05-25 14:35:04 +0200

    Passage au Jquery

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 var gameArea = {
     canvas : document.getElementsByTagName("canvas");
     start : function() {
-        
+        this.context = this.canvas.getContext("2d");
+        this.interval = setInterval()
     }
 }
 
@@ -10,4 +11,5 @@ function setGame() {
      * Récupère les options
      * et lance une partie en fonction
      */
+    console.log(choixMode.options[choixMode.selected])
 }
