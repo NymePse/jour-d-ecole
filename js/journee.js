38d679988cd04c3bb8b802314b9c39b56a779753
Pseudo_Nyme <arnaud.bijou@gmail.com>
2020-06-01 14:29:07 +0200

    Description Journée

--- js/journee.js
+++ js/journee.js
@@ -1,6 +1,22 @@
-function journee(exercice, difficulte, chrono) {
-    /*
-     * Joue tout les modes de jeu
-     * dans l'ordre, avec des transitions
-     */
-}
+//SECTION : Journée
+/*
+* Suite des modes de jeu avec des transitions entre chacun.
+*
+* Déroulement :
+*   introduction de la journée   
+*   lancer jeu Chemin
+*   texte transition avec foot selon score 
+*   lancer jeu foot
+*   texte transition soleil
+*   texte fin de jeu
+*/
+
+/*
+ * Découpage :
+ * 
+ * Set up : variables de journée, texte d'intro, lance Chemin
+ * pré-foot : texte de transition, lance Foot
+ * pré-soleil : texte de transition, lance Soleil
+ * conclusion : texte de fin, score total...
+ * 
+ */
